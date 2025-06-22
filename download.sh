2d71f2b201f878e019a62ae7ab45d46f9ee53ffe
Seth G. R. Herendeen <inivican@hotmail.com>
2025-06-21 20:12:12 -0400

    Update download.sh
    handle file name lengths that are too long by using output template that inserts the uploader and the id of the video

--- download.sh
+++ download.sh
@@ -13,4 +13,6 @@ echo "Input URL:";
 read video 
 
 #download whatever is at the URL and convert it to .mp4 if not already
-bin/yt-dlp -f mp4 $video 
+#using -o instead of --trim-filenames because on some unix-like filesystems the filename length 
+#refers to length in bytes and not length of strings
+bin/yt-dlp -f mp4 $video -o "%(uploader)s_%(id)s.%(ext)s"
