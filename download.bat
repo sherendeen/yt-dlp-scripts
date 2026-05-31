ff5fce64d1e6998a6fcd48099fc0ff5c242f4f81
Seth G. R. Herendeen <inivican@hotmail.com>
2026-05-31 15:39:22 -0400

    improved command-line capability
    there are still issues with the args functionality processing playlists specifically, this seems to be because of how cmd processes certain urls "https://www.youtube.com/playlist?list=PLoy30KiW3lTfD-ROKr8ltNzhBzE-URgux" -> is treated as "https://www.youtube.com/playlist?list" and then after the = in the url is treated as a separate arg. I am loooking into fixing this. The command args still work for individual videos

--- download.bat
+++ download.bat
@@ -1,7 +1,32 @@
 @echo off
 
+
 REM This script is in the public domain
 REM Originally by Seth G. R. Herendeen
+
+IF %1.==. GOTO No1
+REM PROGRAM IS EXPECTING AN AUDIO FORMAT AS FIRST ARG. YT-DLP 
+REM WILL NOT PERMIT PASSING OF A VIDEO FORMAT INSTEAD.
+IF %2.==. GOTO No2
+IF %3.==. GOTO No3
+
+echo %1 ... %2 ... %3 (default: 16)
+SET LENGTH=%3
+GOTO ctr
+
+:No1
+  ECHO No param 1
+GOTO start
+:No2
+  ECHO No param 2
+GOTO start
+:No3
+  ECHO No param 3 (filename length). Fallback to default of 16.
+  SET LENGTH=16
+
+:ctr
+bin\yt-dlp -x --audio-format %1 --trim-filenames %LENGTH% "%2"
+goto EOF
 
 :start
 echo Input link representing video to be downloaded
@@ -19,21 +44,30 @@ if %option%=="2" GOTO dl_vorbis
 if %option%=="3" GOTO dl_mp3
 if %option%=="4" GOTO dl
 :dl_flac
-bin\yt-dlp -x --audio-format --trim-filenames 16 flac %video%
+bin\yt-dlp -x --audio-format flac --trim-filenames 16  %video%
 GOTO EOF
 
 :dl_vorbis
-bin\yt-dlp -x --audio-format --trim-filenames 16 vorbis %video%
+bin\yt-dlp -x --audio-format vorbis --trim-filenames 16  %video%
 GOTO EOF
 
 :dl_mp3
 bin\yt-dlp -x -F mp3 --trim-filenames 16 --audio-format mp3 %video%
 GOTO EOF
 
+:dl_noconvert
+SET video=%2
+SET LENGTH=%3
+bin\yt-dlp -k --trim-filenames %LENGTH% %video%
+
+GOTO EOF
+
 :dl
 bin\yt-dlp -k --trim-filenames 16 %video%
 
+
 :EOF
+pause
 echo.
 echo.
 echo Done!
